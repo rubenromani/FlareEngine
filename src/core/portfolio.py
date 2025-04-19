83793968e9dceb4e9d85bb6720ef1d18aea239cc
rubenromani <ruben.romani@outlook.it>
2025-04-19 16:40:15 +0200

    Added SharedRepository

--- src/core/portfolio.py
+++ src/core/portfolio.py
@@ -1,16 +1,23 @@
 from src.logging.logger_provider import get_logger
+from src.core.dispatcher import Dispatcher
+from src.core.shared_repository import SharedRepository
 
 logger = get_logger(__name__, "DEBUG")
 
 class Portfolio:
 
     def __init__(self):
-        self.balance = 100000 # Hardcoded in this moment
+        self.balance = 100000.0 # Hardcoded in this moment
+        self.available_balance = self.balance # balance - balance frozen for pending orders
         self.equity = self.balance
         self.last_prices = {}
         self.positions = {}
         self.transaction = []
+
+        self.dispatcher = Dispatcher()
+        self.repo = SharedRepository()
         
+        self.repo.set("available_balance", self.available_balance);
 
 
         
---
+++ src/core/shared_repository.py
@@ -0,0 +1,31 @@
+import threading
+
+class SharedRepository:
+    _instance = None
+ 
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(SharedRepository, cls).__new__(cls)
+            cls._instance.data = {}
+            cls._instance.lock = threading.Lock()
+        return cls._instance
+    
+    def set(self, key, value):
+        with self.lock:
+            self.data[key] = value
+
+    def get(self, key, default=None):
+        with self.lock:
+            return self.data.get(key, default)
+        
+    def delete(self, key):
+        with self.lock:
+            if key in self.data:
+                del self.data[key]
+
+    def contains(self, key):
+        with self.lock:
+            return key in self.data
+        
+        
+    
